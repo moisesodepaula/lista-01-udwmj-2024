eec9c07b9880f599c84504b897b65e8efc0f9a0a
moisesodepaula <129560827+moisesodepaula@users.noreply.github.com>
2024-08-30 21:49:13 -0300

    Create media.php

---
+++ media.php
@@ -0,0 +1,14 @@
+<?php
+// media.php
+
+// Define três variáveis com as notas
+$nota1 = 7.5;
+$nota2 = 8.0;
+$nota3 = 6.5;
+
+// Calcula a média das notas
+$media = ($nota1 + $nota2 + $nota3) / 3;
+
+// Exibe o resultado
+echo "A média das notas $nota1, $nota2 e $nota3 é $media.";
+?>
